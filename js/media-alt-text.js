cac33b06db108b4ab6884f1dc713148944a4a261
Utkarsh Patel <itismeutkarsh@gmail.com>
2024-04-14 18:56:06 +0530

    Format

--- js/media-alt-text.js
+++ js/media-alt-text.js
@@ -1,74 +1,76 @@
 /* global betterFileName */
 import domReady from '@wordpress/dom-ready';
 
-domReady(() => {
+domReady( () => {
 	function addButtonHandler() {
-		const button = document.querySelector('.generate-alt-text');
-		if (button) {
-			button.addEventListener('click', async function () {
-				const mediaId = button.getAttribute('data-media-id');
+		const button = document.querySelector( '.generate-alt-text' );
+		if ( button ) {
+			button.addEventListener( 'click', async function () {
+				const mediaId = button.getAttribute( 'data-media-id' );
 				const input = document.querySelector(
 					'#attachment-details-two-column-alt-text'
 				);
 				button.style.display = 'none';
-				const spinner = button.parentNode.querySelector('.spinner');
-				const generateAltText = button.parentNode.querySelector('.generate-alt-text__loading');
+				const spinner = button.parentNode.querySelector( '.spinner' );
+				const generateAltText = button.parentNode.querySelector(
+					'.generate-alt-text__loading'
+				);
 				spinner.style.visibility = 'visible';
-				generateAltText.classList.remove('hidden');
+				generateAltText.classList.remove( 'hidden' );
 
 				const altText = input.value;
 				return;
-				if (betterFileName?.api) {
+				if ( betterFileName?.api ) {
 					try {
-						const result = await fetch(betterFileName.api, {
+						const result = await fetch( betterFileName.api, {
 							method: 'POST',
 							headers: {
 								'Content-Type': 'application/json',
 								'X-WP-Nonce': betterFileName.nonce,
 							},
-							body: JSON.stringify({
+							body: JSON.stringify( {
 								mediaId,
 								altText,
-							}),
-						});
+							} ),
+						} );
 						const data = await result.json();
-						if (data.alt_text) {
+						if ( data.alt_text ) {
 							input.value = data.alt_text;
-							const event = new Event('change', {
+							const event = new Event( 'change', {
 								bubbles: true,
 								cancelable: true,
-							});
-							input.dispatchEvent(event);
+							} );
+							input.dispatchEvent( event );
 						}
-					} catch (error) {
-						console.error('Error:', error);
+					} catch ( error ) {
+						console.error( 'Error:', error );
 					} finally {
-						generateAltText.classList.add('hidden');
+						generateAltText.classList.add( 'hidden' );
 						spinner.style.visibility = 'hidden';
 						button.style.display = 'inline-block';
 					}
 				}
-			});
+			} );
 		}
 	}
 
 	const ModalView = wp.media.view.Modal;
-	wp.media.view.Modal = wp.media.view.Modal.extend({
+	wp.media.view.Modal = wp.media.view.Modal.extend( {
 		open() {
 			// Ensure that the main attachment fields are rendered.
-			ModalView.prototype.open.apply(this);
+			ModalView.prototype.open.apply( this );
 			// debugger;
 			addButtonHandler();
 		},
-	});
+	} );
 
 	const EditAttachments = wp.media.view.MediaFrame.EditAttachments;
 	wp.media.view.MediaFrame.EditAttachments =
-		wp.media.view.MediaFrame.EditAttachments.extend({
+		wp.media.view.MediaFrame.EditAttachments.extend( {
 			rerender() {
-				EditAttachments.prototype.rerender.apply(this, arguments);
+				EditAttachments.prototype.rerender.apply( this, arguments );
 				// debugger;
 				addButtonHandler();
 			},
-		});
-});
+		} );
+} );
